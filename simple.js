43cdd2e1eacf4a3131339751d6f4763b7a3e1795
Sooraj-me <soorajshagu@gmail.com>
2025-09-03 22:53:30 +0530

    first commit

---
+++ simple.js
@@ -0,0 +1,18 @@
+const texts = ["Full Stack Developer", "Frontend Specialist", "Backend Expert"];
+let count = 0, index = 0, currentText = "", letter = "";
+
+function type() {
+  if (count === texts.length) count = 0;
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.getElementById("typing-text").textContent = letter;
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+    setTimeout(type, 1500);
+  } else {
+    setTimeout(type, 100);
+  }
+}
+type();
